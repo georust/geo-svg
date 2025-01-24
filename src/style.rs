2d8353914932e0cd6d7570c451e8ab6edc18026a
Gustave Coulombe <magikgus@gmail.com>
2025-01-23 21:22:37 -0500

    Added doc comments for LineCap and LineJoin

--- src/style.rs
+++ src/style.rs
@@ -1,16 +1,57 @@
 use crate::Color;
 use std::fmt::{Display, Formatter, Result};
 
+/**
+LineCap is used to define the shape to be used at the end of strokes.
+
+Example:
+```
+use geo::Point;
+use geo_svg::{Color, LineCap, ToSvg};
+
+let point = geo::Point::new(0.0, 0.0);
+let svg_point = point
+    .to_svg()
+    .with_radius(20.0)
+    .with_fill_opacity(0.0)
+    .with_stroke_color(Color::Named("blue"))
+    .with_stroke_width(1.0)
+    .with_stroke_linecap(LineCap::Round)
+    .with_stroke_dasharray(vec![2.0, 1.0, 3.0, 1.0]);
+```
+*/
 #[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub enum LineCap {
-   #[default]
+    #[default]
     Butt,
     Round,
     Square,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+/**
+LineJoin is used to define the shape to be used at the corners of strokes.
+
+Example:
+```
+use geo::{Triangle};
+use geo_svg::{Color, LineJoin, ToSvg};
+
+let triangle = Triangle::new(
+    geo::Coord { x: 0.0, y: 0.0 },
+    geo::Coord { x: 10.0, y: 0.0 },
+    geo::Coord { x: 5.0, y: 10.0 },
+);
+let svg_triangle = triangle
+    .to_svg()
+    .with_fill_opacity(0.5)
+    .with_color(Color::Named("red"))
+    .with_stroke_width(2.0)
+    .with_stroke_linejoin(LineJoin::Bevel);
+```
+*/
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
 pub enum LineJoin {
+    #[default]
     Miter,
     Round,
     Bevel,
